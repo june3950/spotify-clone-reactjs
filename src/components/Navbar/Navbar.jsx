4ad67ddbcca6ef3e712cebadb94c861719d19218
june3950 <jun3950@korea.ac.kr>
2020-10-15 23:04:35 +0900

    아바타 가림

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import SearchInput from '../SearchInput/SearchInput';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import SearchInput from "../SearchInput/SearchInput";
+import { useSelector } from "react-redux";
 import {
   NavContainer,
   NavRightWrapper,
@@ -13,14 +13,14 @@ import {
   NavSubcontainer,
   ArrowsContainer,
   ArrowIconContainer,
-  UserContainer
-} from './navbarStyles';
-import { ReactComponent as ArrowDown } from '../../assets/icons/down-arrow.svg';
-import { ReactComponent as ArrowLeft } from '../../assets/icons/arrow-left.svg';
-import { ReactComponent as ArrowRight } from '../../assets/icons/arrow-right.svg';
-import { useLocation, useHistory } from 'react-router-dom';
-import NavbarProfileMenu from './NavbarProfileMenu';
-import LibraryMenu from '../LibraryMenu/LibraryMenu';
+  UserContainer,
+} from "./navbarStyles";
+import { ReactComponent as ArrowDown } from "../../assets/icons/down-arrow.svg";
+import { ReactComponent as ArrowLeft } from "../../assets/icons/arrow-left.svg";
+import { ReactComponent as ArrowRight } from "../../assets/icons/arrow-right.svg";
+import { useLocation, useHistory } from "react-router-dom";
+import NavbarProfileMenu from "./NavbarProfileMenu";
+import LibraryMenu from "../LibraryMenu/LibraryMenu";
 
 const Navbar = () => {
   const { images, display_name } = useSelector(({ auth }) => auth.user);
@@ -34,34 +34,34 @@ const Navbar = () => {
         <NavLeftWrapper>
           <ArrowsContainer>
             <ArrowIconContainer onClick={() => history.goBack()}>
-              <ArrowLeft fill='#fff' width={22} height={22} />
+              <ArrowLeft fill="#fff" width={22} height={22} />
             </ArrowIconContainer>
             <ArrowIconContainer onClick={() => history.goForward()}>
-              <ArrowRight fill='#fff' width={22} height={22} />
+              <ArrowRight fill="#fff" width={22} height={22} />
             </ArrowIconContainer>
           </ArrowsContainer>
-          {pathname === '/app/search' && <SearchInput />}
+          {pathname === "/app/search" && <SearchInput />}
           {[
-            '/app/collection/playlists',
-            '/app/collection/albums',
-            '/app/collection/artists'
+            "/app/collection/playlists",
+            "/app/collection/albums",
+            "/app/collection/artists",
           ].includes(pathname) && <LibraryMenu />}
         </NavLeftWrapper>
         <NavRightWrapper>
           <UpgradeButton
-            href='https://www.spotify.com/premium'
-            target='_blank'
-            rel='noopener noreferrer'
-            title='Upgrade to Premium'
+            href="https://www.spotify.com/premium"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Upgrade to Premium"
           >
             <UpgradeText>upgrade</UpgradeText>
           </UpgradeButton>
 
           <UserContainer onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            {images && <Avatar src={images[0].url} />}
+            {/* {images && <Avatar src={images[0].url} />} */}
             <Name>{display_name}</Name>
             <ArrowDownIconContainer open={isMenuOpen}>
-              <ArrowDown fill='#fff' width={10} height={10} />
+              <ArrowDown fill="#fff" width={10} height={10} />
             </ArrowDownIconContainer>
             <NavbarProfileMenu open={isMenuOpen} />
           </UserContainer>
